8b41c52fde42ccdaaacdd3ce5b145c0df0af81a3
KazimirKozulia <kazkozsquad@gmail.com>
2025-05-21 14:17:44 +0300

    added App

---
+++ DisputeApp/DisputeApp/DisputeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DisputeAppApp.swift
+//  DisputeApp
+//
+//  Created by Kazimir Kozulia on 21.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct DisputeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
